36a8cf6b680314814eda6d61a8b12e828364a91e
Aron van Ammers <aron@blockstars.io>
2017-01-25 15:24:17 +0000

    Wakefield export script

---
+++ scripts/export/export_wakefield_import.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rethinkdb export -e bigchain.wakefield_import --format csv --fields organisation_name,organisation_code,effective_date,directorate,sercop_service,sercop_uri,supplier_name,buyer_supplier_id,company_registration,charity_registration,vcse_grant,payment_date,transaction_number,sequence_number,amount_net,amount_irrecoverable_vat,purpose,type,procurement_classification_1,class_code_1,procurement_classification_2,class_code_2,thomson_classification,thomson_description,contract_number,service_division,supplier_classification_1
