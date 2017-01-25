da36d58af16bc8686bae6cbe9e7769cd936730a9
Aron van Ammers <aron@blockstars.io>
2017-01-25 20:52:21 +0000

    Trim strings in preprocess

--- scripts/import/doncaster/2015_jul-oct/import_doncaster_file.sh
+++ scripts/import/doncaster/2015_jul-oct/import_doncaster_file.sh
@@ -2,6 +2,7 @@
 
 echo Importing doncaster file $1...
 
-cat "$1" > "$1.preprocessed"
+# Trim spaces (RethinkDB is not good at that)
+cat "$1" | sed 's/        //g' | sed 's/    //g' | sed 's/    //g' | sed 's/  //g' | sed 's/  //g' | sed 's/  //g' | sed 's/  //g' | sed 's/ ,/,/g' | sed 's/ \$//g' > $1.preprocessed
 
 ../../rethinkdb_import.sh "$1.preprocessed" bigchain.doncaster_import --custom-header effective_date,transaction_number,directorate,sercop_service,service_division,supplier_name,procurement_classification_1,procurement_classification_2,amount_net,supplier_classification_1
