c6187e1f6c72d82b3ac78d6e889e355df856e7c2
Aron van Ammers <aron@blockstars.io>
2017-01-25 10:33:49 +0000

    Correct path

--- scripts/fetch/doncaster.sh
+++ scripts/fetch/doncaster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./download-csvs.sh doncaster/2014 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2014-15
-./download-csvs.sh doncaster/2015 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2015-16
-./download-csvs.sh doncaster/2016 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2016-17
+./download-csvs.sh data/doncaster/2014 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2014-15
+./download-csvs.sh data/doncaster/2015 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2015-16
+./download-csvs.sh data/doncaster/2016 2 http://www.doncaster.gov.uk/services/the-council-democracy/payments-to-suppliers-reports-2016-17
 
