f3397ab9daa5de34f6566c02028c88dbc3bc7674
Aron van Ammers <aron@blockstars.io>
2017-01-25 14:00:27 +0000

    Correct field sequence

--- scripts/import/doncaster/import_doncaster_all.sh
+++ scripts/import/doncaster/import_doncaster_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./import_doncaster_dir.sh ../../fetch/data/doncaster/2015_july_onwards/2015
-./import_doncaster_dir.sh ../../fetch/data/doncaster/2015_july_onwards/2016
+./import_doncaster_dir.sh ../../fetch/data/doncaster/2015_nov_onwards/2015
+./import_doncaster_dir.sh ../../fetch/data/doncaster/2015_nov_onwards/2016
--- scripts/import/doncaster/import_doncaster_file.sh
+++ scripts/import/doncaster/import_doncaster_file.sh
@@ -4,4 +4,4 @@ echo Importing doncaster file $1...
 
 cat "$1" > "$1.preprocessed"
 
-../rethinkdb_import.sh "$1.preprocessed" bigchain.doncaster_import --custom-header effective_date,transaction_number,directorate,sercop_service,supplier_classification_1,procurement_classification_1,procurement_classification_2,supplier_name,service_division
+../rethinkdb_import.sh "$1.preprocessed" bigchain.doncaster_import --custom-header effective_date,transaction_number,directorate,sercop_service,supplier_classification_1,procurement_classification_1,procurement_classification_2,supplier_name,amount_net
