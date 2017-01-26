8f202469212796338d3193b3278838c62c819006
Aron van Ammers <aron@blockstars.io>
2017-01-26 18:06:54 +0000

    Hide details and show with button

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -5,10 +5,26 @@ import template from './navigation.html';
 
 const name = 'navigation';
 
+class NavigationController {
+  constructor($scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    $scope.helpers({
+      isLoggedIn: function () {
+        return Meteor.userId() != null;
+
+      }
+    });
+  }
+}
+
 // create a module
 export default angular.module(name, [
   angularMeteor
 ]).component(name, {
   template,
-  controllerAs: name
+  controllerAs: name,
+  controller: NavigationController
 });
