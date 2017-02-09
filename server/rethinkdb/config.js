d78f0672cd42402ce2e6c31cd4d563a4038f7704
Aron van Ammers <aron@blockstars.io>
2017-02-09 09:30:35 +0000

    Migrate rethinkDB collections to Mongo, WIP

--- server/rethinkdb/config.js
+++ server/rethinkdb/config.js
@@ -1,6 +1,14 @@
-
 export const Config = {
     host: process.env.RETHINKDB_HOST || 'localhost',
     port: 28015,
-    db: 'bigchain'
+    db: 'bigchain',
+    bigchainDb: {
+        database: {
+            // "mongodb" or "rethinkdb" (not supported)
+            type: "mongodb",
+            url: process.env.MONGO_BIGCHAINDB_URL || 'mongodb://localhost/bigchain',
+        },
+        // API of the BigchainDB service
+        apiUrl: "",
+    }
 }
