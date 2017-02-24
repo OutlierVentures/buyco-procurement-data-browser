c1b32058727e2bc64ce6f4a8160d2e243e302722
Aron van Ammers <aron@blockstars.io>
2017-02-24 16:44:42 +0000

    Add Calderdale fetch script

---
+++ scripts/fetch/calderdale.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./download-csvs.sh data/calderdale/2016 2 http://www.calderdale.gov.uk/council/finances/income-spending/index.jsp?year=2016
+./download-csvs.sh data/calderdale/2015 2 http://www.calderdale.gov.uk/council/finances/income-spending/index.jsp?year=2015
+
