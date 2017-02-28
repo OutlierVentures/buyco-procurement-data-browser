3fcf19ec0ffbed8f6a84541f731eae41bd453d73
Aron van Ammers <aron@blockstars.io>
2017-02-28 20:06:39 +0000

    Better way of postprocessing Trifacta output

--- private/scripts/post-process/postprocess_trifacta.js
+++ private/scripts/post-process/postprocess_trifacta.js
@@ -25,27 +25,30 @@
 function updateTrifactaData() {
     // Convert effective_date to date
     db.public_spending
-        // .find({ organisation_name: "The Borough of Calderdale" })
-        .find()
+        .find({ effective_date: { $type: "string" } })
         .forEach(function (doc) {
-            if (typeof doc.effective_date !== 'string')
-                return;
-
             var parts = doc.effective_date.split("/");
             var dt = new Date(parseInt(parts[2], 10),
                 parseInt(parts[1], 10) - 1,
                 parseInt(parts[0], 10));
-            db.public_spending.update({ _id: doc._id }, { "$set": { effective_date: dt, payment_date: dt } });
+            db.public_spending.update({ _id: doc._id }, { "$set": { effective_date: dt } });
+        });
+
+    // payment_date to date
+    db.public_spending
+        .find({ payment_date: { $type: "string" } })
+        .forEach(function (doc) {
+            var parts = doc.payment_date.split("/");
+            var dt = new Date(parseInt(parts[2], 10),
+                parseInt(parts[1], 10) - 1,
+                parseInt(parts[0], 10));
+            db.public_spending.update({ _id: doc._id }, { "$set": { payment_date: dt } });
         });
 
     // Convert amount to number
     db.public_spending
-        // .find({ organisation_name: "The Borough of Calderdale" })
-        .find()
+        .find({ amount_net: { $type: "string" } })
         .forEach(function (doc) {
-            if (typeof doc.amount_net !== 'string')
-                return;
-
             db.public_spending.update({ _id: doc._id }, { "$set": { amount_net: Number(doc.amount_net) } });
         });
 }
