a1b2df2102af642c2a5f0c556febd63b925f0375
Aron van Ammers <aron@blockstars.io>
2017-03-27 22:09:25 +0200

    Add Hotjar tracking code

--- client/main.js
+++ client/main.js
@@ -37,3 +37,18 @@ Meteor.startup(function(){
   ga('send', 'pageview');
 
 });
+
+
+/**
+ * Load Hotjar tracker.
+ */
+Meteor.startup(function(){
+  (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:457085,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+});
