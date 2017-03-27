55d8d2108464f39e4c548b79e242583a27dbbf15
Aron van Ammers <aron@blockstars.io>
2017-03-27 22:04:30 +0200

    Add Google Analytics

--- client/main.js
+++ client/main.js
@@ -17,3 +17,23 @@ if (Meteor.isCordova) {
 } else {
   angular.element(document).ready(onReady);
 }
+
+
+/**
+ * Load Google Analytics tracker.
+ */
+Meteor.startup(function(){
+  // To disable Google analytics, uncomment the line below.
+  // TODO: use Meteor.settings to enable/disable.
+  // return;
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  // UA-76730152-2 - PDW staging app
+  ga('create', 'UA-76730152-2', 'auto');
+  ga('send', 'pageview');
+
+});
