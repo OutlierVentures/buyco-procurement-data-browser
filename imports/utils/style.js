0e9393e856b33427ee25b5e1d28b32b4f82c36fe
Aron van Ammers <aron@blockstars.io>
2017-04-26 16:09:10 +0200

    Document color generator function

--- imports/utils/style.js
+++ imports/utils/style.js
@@ -1,13 +1,20 @@
 
-function rgb(string){
-    return string.match(/\w\w/g).map(function(b){ return parseInt(b,16) })
-}
 /**
- * Get a unique HTML colour code derived from the provided string.
+ * Parse an HTML colour into its RGB components as integers.
+ * 
+ * @param {*HTML colour, for example "#ffaabb"} string 
+ */
+function rgb(string) {
+    return string.match(/\w\w/g).map(function (b) { return parseInt(b, 16) })
+}
+
+/**
+ * Get a unique HTML colour code derived from the provided string, and based on a set of 
+ * palette colours.
  */
 export const stringToColour = (str) => {
     // Determine a simple numeric hash of the string.
-    let hash = 0;        
+    let hash = 0;
 
     for (let i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -16,55 +23,59 @@ export const stringToColour = (str) => {
     // We need an absolute integer as input to the colour choosing algorithm.
     hash = Math.abs(hash);
 
-    console.log("Hash", hash);
+    // Our colour palette. We choose two of these colours as our range, then pick a colour from that range.
+    // let colours = ["#F49814", "#FBC100", "#DB5461", "#686963", "#78C7C8"];
+    let colours = ["#ff0000", "#0000ff"];
 
-    // The ranges are chosen from these colours.            
-    let colours = ["#F49814", "#FBC100", "#DB5461", "#686963", "#78C7C8"];
-
-    // Get the two colours that determine our range.           
+    // Get the two colours that determine our range. In the comments we assume a palette of 5 colours.
 
     // Colour 1: the remainder of our hash divided by 5 to get an integer in 0..4
     let cIndex1 = hash % colours.length;
 
-    // Colour 2: [index of colour 1 (0..4)] + [a deterministic value between 0 and 3 plus 1], again taking
-    // the remainder to end up with 0..4 (but a different value than colour 1)
+    // Colour 2: [index of colour 1 (0..4)] + [a deterministic value in 0..3 plus 1], again taking
+    // the remainder of that to end up with 0..4 (but a different value than colour 1)
     let cIndex2 = ((hash * 7) % (colours.length - 1) + 1 + cIndex1) % colours.length;
 
     let colour1 = colours[cIndex1];
     let colour2 = colours[cIndex2];
 
-    console.log("Picking a color between:");
-    console.log("%c" + colour1, "color: " + colour1);
-    console.log("%c" + colour2, "color: " + colour2);
+    // console.log("Picking a color between:");
+    // console.log("%c" + colour1, "color: " + colour1);
+    // console.log("%c" + colour2, "color: " + colour2);
+
     let rgb1 = rgb(colour1);
     let rgb2 = rgb(colour2);
 
     var newRgb = [];
-    
-    // Adapted from: http://stackoverflow.com/a/23277879/81949
-    for (var i=0; i<3; i++) {
-        let diff = Math.abs(rgb2[i]-rgb1[i]);
 
-        if (diff == 0)
-        {
+    // Adapted from: http://stackoverflow.com/a/23277879/81949
+    for (var i = 0; i < 3; i++) {
+        // For example if colour 1 has an R value of 240 and colour 2 has an R value of 100,
+        // we want to get a deterministic value in 100..240.
+
+        // Get the absolute difference between the colour component in color 1 and 2.
+        let diff = Math.abs(rgb2[i] - rgb1[i]);
+
+        if (diff == 0) {
             newRgb[i] = rgb1[i];
             continue;
         }
 
+        // Get the divide remainder from our hash code to determine, for example 50.
         let hashRemainder = hash % diff;
 
-        if(rgb2[i] > rgb1[i])
-            newRgb[i] = rgb1[i] + hashRemainder | 0;
-        else
-            newRgb[i] = rgb2[i] + hashRemainder | 0;
+        // Now find the new value for the colour component by adding the remainder to
+        // the lowest of the two values.
+        newRgb[i] = Math.min(rgb1[i], rgb2[i]) + hashRemainder | 0;
     }
 
-    let newHtmlColor = '#' + newRgb
-        .map(function(n){ return n.toString(16) })
-        .map(function(s){ return "00".slice(s.length)+s}).join(''); 
+    // Convert back to HTML colour format and return.
+    let newColour = '#' + newRgb
+        .map(function (n) { return n.toString(16) })
+        .map(function (s) { return "00".slice(s.length) + s }).join('');
 
-    console.log("Result:");
-    console.log("%c" + newHtmlColor, "color: " + newHtmlColor);
+    // console.log("Result:");
+    // console.log("%c" + newHtmlColor, "color: " + newHtmlColor);
 
-    return newHtmlColor;
+    return newColour;
 }
