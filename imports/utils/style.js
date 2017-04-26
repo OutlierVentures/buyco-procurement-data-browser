a2591d836c2e55b2e708379a73d1204d9fa0cfe8
Aron van Ammers <aron@blockstars.io>
2017-04-26 16:42:40 +0200

    Adapt colour generator to use gradient

--- imports/utils/style.js
+++ imports/utils/style.js
@@ -24,8 +24,11 @@ export const stringToColour = (str) => {
     hash = Math.abs(hash);
 
     // Our colour palette. We choose two of these colours as our range, then pick a colour from that range.
+    // TODO: move to a config file.
     // let colours = ["#F49814", "#FBC100", "#DB5461", "#686963", "#78C7C8"];
-    let colours = ["#ff0000", "#0000ff"];
+    let colours = ["#F49814", "#FBC100", "#DB5461", "#78C7C8"];
+    // Bright red and blue for testing purposes
+    // let colours = ["#ff0000", "#0000ff"];
 
     // Get the two colours that determine our range. In the comments we assume a palette of 5 colours.
 
@@ -49,24 +52,47 @@ export const stringToColour = (str) => {
     var newRgb = [];
 
     // Adapted from: http://stackoverflow.com/a/23277879/81949
-    for (var i = 0; i < 3; i++) {
-        // For example if colour 1 has an R value of 240 and colour 2 has an R value of 100,
-        // we want to get a deterministic value in 100..240.
+    // for (var i = 0; i < 3; i++) {
+    //     // For example if colour 1 has an R value of 240 and colour 2 has an R value of 100,
+    //     // we want to get a deterministic value in 100..240.
 
+    //     // Get the absolute difference between the colour component in color 1 and 2.
+    //     let diff = Math.abs(rgb2[i] - rgb1[i]);
+
+    //     if (diff == 0) {
+    //         newRgb[i] = rgb1[i];
+    //         continue;
+    //     }
+
+    //     // Get the divide remainder from our hash code to determine, for example 50.
+    //     let hashRemainder = hash % diff;
+
+    //     // Now find the new value for the colour component by adding the remainder to
+    //     // the lowest of the two values.
+    //     newRgb[i] = Math.min(rgb1[i], rgb2[i]) + hashRemainder | 0;
+    // }
+
+    // Pure gradient variant: calculate a deterministic int in 0..255, then take that as the
+    // relative point for all three colour components.
+    let hashRemainder = hash % 256;
+
+    for (var i = 0; i < 3; i++) {
         // Get the absolute difference between the colour component in color 1 and 2.
-        let diff = Math.abs(rgb2[i] - rgb1[i]);
+        let diff = rgb2[i] - rgb1[i];
 
         if (diff == 0) {
             newRgb[i] = rgb1[i];
             continue;
         }
 
-        // Get the divide remainder from our hash code to determine, for example 50.
-        let hashRemainder = hash % diff;
+        // Compute the relative difference. For example if colour 1 has an R value of 
+        // 240 and colour 2 has an R value of 100, our diff is 140 and we want to get 
+        // a value in 0..140 to add to 100.
+        let relDiff = Math.round( diff * hashRemainder / 256 );
 
         // Now find the new value for the colour component by adding the remainder to
-        // the lowest of the two values.
-        newRgb[i] = Math.min(rgb1[i], rgb2[i]) + hashRemainder | 0;
+        // the colour 1 component.
+        newRgb[i] = rgb1[i] + relDiff | 0;
     }
 
     // Convert back to HTML colour format and return.
