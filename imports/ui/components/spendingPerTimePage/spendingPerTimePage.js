e91cf0f97897ab51cf268955051feafc6e952bd1
Aron van Ammers <aron@blockstars.io>
2017-04-26 13:05:44 +0200

    Comment stringToColour function

--- imports/ui/components/spendingPerTimePage/spendingPerTimePage.js
+++ imports/ui/components/spendingPerTimePage/spendingPerTimePage.js
@@ -735,11 +735,18 @@ class SpendingPerTimePage {
             }
         });
 
+        /**
+         * Get a unique HTML colour code derived from the provided string.
+         */
         function stringToColour (str) {
-            let hash = 0;
+            // Determine a simple numeric hash of the string.
+            let hash = 0;        
+
             for (let i = 0; i < str.length; i++) {
                 hash = str.charCodeAt(i) + ((hash << 5) - hash);
-            }
+            }            
+
+            // Build a HTML colour code by deriving 3 16-bit values (r, g, b) from the hash.
             let colour = '#';
             for (let i = 0; i < 3; i++) {
                 let value = (hash >> (i * 8)) & 0xFF;
