2b19e8177d7b18ea4f5226d83f324a4e68e9c0d9
Aron van Ammers <aron@blockstars.io>
2017-05-01 19:40:12 +0200

    Load data for multiple regression

--- server/predictions/methods.js
+++ server/predictions/methods.js
@@ -1,13 +1,49 @@
-import { PredictionRuns} from '/imports/api/predictionRuns';
-import {Predictions} from '/imports/api/predictions';
-import {Spending} from '/server/bigchaindb/spending';
+import { PredictionRuns } from '/imports/api/predictionRuns';
+import { Predictions } from '/imports/api/predictions';
+import { Spending } from '/server/bigchaindb/spending';
 
 export function executePredictionStep(predictionRunId) {
     if (Meteor.isServer) {
-        console.log("executePredictionStep");
+        console.log("executePredictionStep", predictionRunId);
+
+        // Load data for all councils, all time
+
+        let data = [];
+        // const spendingRaw = Spending.rawCollection();
+        // const findSpending = Meteor.wrapAsync(spendingRaw.find, Spending);
+
+        // var cursor = findSpending({}, { organisation_name: 1, procurement_classification_1: 1, amount_net: 1 })
+
+        var cursor = Spending.find({
+            organisation_name: "Wakefield MDC"
+        }, {
+                fields: { organisation_name: 1, procurement_classification_1: 1, amount_net: 1, payment_date: 1 }
+                // , limit: 10 
+            });
+
+        //console.log("data length", cursor.length);
+        cursor.forEach((doc) => {
+            doc.month = new Date(doc.payment_date.getFullYear(), doc.payment_date.getMonth(), 1);
+            data.push(doc);
+        });
+
+        console.log("data length after forEach", data.length);
+
+        console.log(data[0]);
+
     }
 }
 
+/**
+ * For a prediction_run, returns a set of parameters for which a prediction is
+ * yet to be generated.
+ * 
+ * @param {*string} predictionRunId 
+ */
+function getFirstUnprocessedParameters(predictionRunId) {
+
+}
+
 Meteor.methods({
-  executePredictionStep
+    executePredictionStep
 });
