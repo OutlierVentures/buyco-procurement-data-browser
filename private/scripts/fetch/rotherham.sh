08cfe289a8d66f93d59627d7278a7e034c6bc6c5
Aron van Ammers <aron@blockstars.io>
2017-05-12 23:08:07 +0200

    Solve case where delete filter button wasn't hidden

--- private/scripts/fetch/rotherham.sh
+++ private/scripts/fetch/rotherham.sh
@@ -1,10 +1,86 @@
 #!/bin/sh
 
 # This gives no download results and stops quickly.
-./download-csvs.sh data/rotherham 2 http://www.rotherham.gov.uk/downloads/200110/council_budgets_and_spending
+# ./download-csvs.sh data/rotherham 2 http://www.rotherham.gov.uk/downloads/200110/council_budgets_and_spending
 
 # This doesn't work either (simulating a search in Downloads)
 
 # http://www.rotherham.gov.uk/site/scripts/google_results.php?q=data%20to%20publish&sites%5B%5D=downloads
 
-# 
+# Individual lines gotten through web scraping
+
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/148/october_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3284/spend_over_500_-_february_2017.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/135/may_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/143/may_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/140/december_2011_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/156/june_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3150/june_2016_data_to_publish.xlsx
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1654/october_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/139/september_2011_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/158/august_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2716/october_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3141/january_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3244/december_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3373/spend_over_500_-_march_2017.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/157/july_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3154/august_2016_data_to_publish.xlsx
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/155/may_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/150/december_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/141/january_2012_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/146/august_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3142/february_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/138/august_2011_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2424/may_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/142/february_2012_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/151/january_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/134/april_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2299/april_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1572/september_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3178/october_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2761/november_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1783/december_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/130/december_2010_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/144/june_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/147/september_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3232/november_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1033/march_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/131/january_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/152/february_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1494/august_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3270/spend_over_500_-_january_2017.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/159/september_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1866/january_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3148/may_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/333/december_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2699/september_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3152/july_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2793/december_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/160/october_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1340/june_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/149/november_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/145/july_2012_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2505/june_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2657/august_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3146/april_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/129/november_2010_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3170/september_2016_data_to_publish.xlsx
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1715/november_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1164/april_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/954/february_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1207/may_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/132/february_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1880/february_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2618/july_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/689/january_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/136/june_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/161/november_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/1376/july_2014_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/3144/march_2016_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/2169/march_2015_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/153/march_2013_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/128/october_2010_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/133/march_2011_data_to_publish.xls
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/137/july_2011_data_to_publish.csv
+./download-csvs.sh data/rotherham 1 http://www.rotherham.gov.uk/download/downloads/id/154/april_2013_data_to_publish.xls
+
